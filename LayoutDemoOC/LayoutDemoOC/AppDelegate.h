d6ff18862e45ec6a1937c30c9828bf12635dc167
Pluto Y <kuaileainits@163.com>
2016-02-29 23:51:09 +0800

    Create new demo for the layout tutorial
    Create new demo for the layout tutorial

---
+++ LayoutDemoOC/LayoutDemoOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LayoutDemoOC
+//
+//  Created by ChipSea on 16/2/29.
+//  Copyright © 2016年 pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LayoutDemoOC/LayoutDemoOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LayoutDemoOC
+//
+//  Created by ChipSea on 16/2/29.
+//  Copyright © 2016年 pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
