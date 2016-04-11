b0c4640c3a7b1d185b32df53091bbab4c101ecf4
Pluto Y <kuaileainits@163.com>
2016-04-12 01:01:36 +0800

    Add files for wrap_content Demo

---
+++ LayoutDemoOC/LayoutDemoOC/AutoLayoutDemo/WrapContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  WrapContentViewController.h
+//  LayoutDemoOC
+//
+//  Created by ChipSea on 16/4/12.
+//  Copyright © 2016年 pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WrapContentViewController : UIViewController
+
+@end
