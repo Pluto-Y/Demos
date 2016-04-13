fbb600b129750e4c30d9a9e1bf5b136938265d85
Pluto Y <kuaileainits@163.com>
2016-04-13 23:16:33 +0800

    添加SizeClass的Demo

---
+++ LayoutDemoOC/LayoutDemoOC/SizeClassDemo/SizeClassViewController.h
@@ -0,0 +1,13 @@
+//
+//  SizeClassViewController.h
+//  LayoutDemoOC
+//
+//  Created by ChipSea on 16/4/13.
+//  Copyright © 2016年 pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SizeClassViewController : UIViewController
+
+@end
---
+++ LayoutDemoOC/LayoutDemoOC/SizeClassDemo/SizeClassViewController.m
@@ -0,0 +1,37 @@
+//
+//  SizeClassViewController.m
+//  LayoutDemoOC
+//
+//  Created by ChipSea on 16/4/13.
+//  Copyright © 2016年 pluto-y. All rights reserved.
+//
+
+#import "SizeClassViewController.h"
+
+@interface SizeClassViewController ()
+
+@end
+
+@implementation SizeClassViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
